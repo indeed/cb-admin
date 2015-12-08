cd94f1d1a4194c5dda67256cf8c0b28aa7e54764
Shun Yu Rao <shunyu.rao@gmail.com>
2015-12-07 21:16:36 -0500

    Visual update + added announcements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
-﻿var app = angular.module('cb-admin', ['cb-admin.services'])
+﻿var app = angular.module('cb-admin', ['cb-admin.services', 'firebase', 'mdl'])
 
-var fireRef = new Firebase("https://cbapp.firebaseio.com/");
+var fireRef = new Firebase("https://cbapp.firebaseio.com/announcements");
 
-app.controller('mainCtrl', function ($scope) {
+app.controller('mainCtrl', function ($scope, $firebaseObject, mdlConfig, timeService) {
 
+    mdlConfig.floating = false;
+    $scope.announcements = {};
+    //$scope.announcements = $firebaseObject(fireRef);
+
+    $scope.parseTime = function (stamp) {
+        return timeService.parseUNIX(stamp);
+    }
+
+    $firebaseObject(fireRef).$bindTo($scope, "announcements");
+
+    $scope.$watch('announcements', function () {
+        componentHandler.upgradeAllRegistered();
+    })
+
+    $scope.meme = function (key, i, text) {
+        $scope.announcements[key][i] = text;
+    }
+
+    $scope.deleteEvent = function (date, i) {
+        $scope.announcements[date].splice(i, 1);
+    }
+
+    $scope.addEvent = function () {
+        if ($scope.newEvent != "" && $scope.newEvent) {
+            var time = moment().startOf('day').format("x");
+            if (!$scope.announcements[time]) {
+                $scope.announcements[time] = [];
+            }
+            $scope.announcements[time].push($scope.newEvent);
+            delete $scope.newEvent;
+        }
+        componentHandler.upgradeAllRegistered();
+    }
+
+    $scope.updateUI = function () {
+        componentHandler.upgradeAllRegistered();
+    }
 });
--- js/services.js
+++ js/services.js
@@ -1,5 +1,24 @@
 ﻿var app = angular.module('cb-admin.services', []);
 
-app.factory('', function () {
+app.factory('timeService', function () {
+    return {
+        parseUNIX: function (stamp) {
+            return moment(stamp, "x").format("dddd MMM Do")
+        }
+    }
 
 })
+
+app.filter('orderObjectBy', function () {
+    return function (items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function (item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if (reverse) filtered.reverse();
+        return filtered;
+    };
+});
